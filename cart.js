52df754117ac64a2fa4a511701ce82a001439ee0
Aegon-the-Conqueror <amanuel.ashenafi@bitscollege.edu.et>
2025-06-19 20:40:19 +0300

    finished basic add and remove cart functionality with tests

--- cart.js
+++ cart.js
@@ -1,40 +1,58 @@
 const Item = require('./item');
-class Cart{
-    constructor(){
-        this.items = [];
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(item, quantity) {
+    if (!(item instanceof Item)) {
+      return 'Only instance of Item can be entered into the cart';
+    }
+    if (item.quantity < quantity) {
+      return 'quantity to be added is morethan quantity available';
     }
 
-    addItem(item, quantity){
-        if(!(item instanceof Item)) {
-        return "Only instance of item can be entered into the cart";
-        }
-        if(item.quantity < quantity) {
-            return `quantity can not be morethan ${item.quantity}`
-        }
+    const existingEntry = this.items.find(e => e.item.id === item.id);
 
-        const existingItem = this.items.find(entry => entry.id == item.id);
-        if(existingItem) {
-            existingItem.quantity += quantity;
-            return('Item quantity in cart updated')
-        } else{
-             this.items.push({item,quantity})
-             return ("New item added successfully")
-        }
-
+    if (existingEntry) {
+      existingEntry.quantity += quantity;
+      return 'item quantity in cart increased';
     }
+
+    this.items.push({ item, quantity });
+    return 'New item added successfully';
+  }
+
+  removeItem(item, quantityToRemove = 1) {
+    const itemId = item instanceof Item ? item.id : item;
+    const idx = this.items.findIndex(e => e.item.id === itemId);
+
+    if (idx === -1) {
+      return 'Item to be removed not found in cart';
+    }
+
+    const entry = this.items[idx];
+
+    if (quantityToRemove >= entry.quantity) {
+      this.items.splice(idx, 1);
+      return 'item removed from cart';
+    }
+
+    entry.quantity -= quantityToRemove;
+    return 'item quantity in cart reduced';
+  }
+
+  emptyCart() {
+    this.items.length = 0;
+  }
+
+  calculateTotal() {
+    return this.items.reduce(
+      (sum, e) => sum + e.item.price * e.quantity,
+      0
+    );
+  }
 }
+
 module.exports = Cart;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- cart.test.js
+++ cart.test.js
@@ -2,23 +2,101 @@ const Cart = require('./cart');
 const Item = require('./item');
 
 describe('Shopping Cart', () => {
-    it("should check if a proper item is inserted", () => {
+    it("should check if a proper item is added to cart", () => {
         const cart = new Cart();
         const item = "I don't belong here"
       
         const response = cart.addItem(item,1)
 
-        expect(response).toEqual("Only instance of item can be entered into the cart")
-
+        expect(response).toEqual("Only instance of Item can be entered into the cart")
+        cart.emptyCart()
     })
-    it("should add an item to a cart", () => {
+
+    it("should add a new item to a cart", () => {
         const cart = new Cart();
         const item = new Item({id:1, name:"airpods", 
         price:5000, quantity:10})
 
         const response = cart.addItem(item, 1)
 
-        expect(response).toEqual("Item added successfully")
+        expect(response).toEqual("New item added successfully")
+        cart.emptyCart();
+    })
+
+    it("should update quantity when an existing item is added", () =>{
+        const cart = new Cart();
+        const item = new Item({id:1, name:"airpods", 
+        price:5000, quantity:10})
+
+        cart.addItem(item, 1)
+        const response = cart.addItem(item,1)
+        
+        expect(response).toEqual("item quantity in cart increased")
+        cart.emptyCart();
+    })
+
+    it("should show an error when removing an item that is not found in cart", () => {
+        const cart = new Cart();
+        const item1 = new Item({id:1, name:"airpods", 
+        price:5000, quantity:10});
+        const item2 = new Item({id:2, name: "charger",
+        price: 1500, quantity: 5})
+
+        cart.addItem(item1, 2)
+        const response = cart.removeItem(item2, 3)
+
+        expect(response).toEqual("Item to be removed not found in cart")
+        cart.emptyCart()
+    })
+
+    it("should show an error when quantity of item to be added is morethan the quantity available", () => {
+        const cart = new Cart();
+        const item = new Item({id:1, name:"airpods", 
+        price:5000, quantity:10})
+
+        const response = cart.addItem(item, 12)
+        expect(response).toEqual("quantity to be added is morethan quantity available")
+        cart.emptyCart()
+    })
+
+    it("should remove an item completely from cart", () => {
+        const cart = new Cart();
+        const item = new Item({id:1, name:"airpods", 
+        price:5000, quantity:10})
+
+        cart.addItem(item, 2)
+        const response = cart.removeItem(item, 2)
+
+        expect(response).toEqual("item removed from cart")
+        cart.emptyCart()
+    })
+
+    it("should reduce quantity of an item in cart", () => {
+        const cart = new Cart();
+        const item = new Item({id:1, name:"airpods", 
+        price:5000, quantity:10})
+
+        cart.addItem(item, 5)
+        const response = cart.removeItem(item, 3)
+
+        expect(response).toEqual("item quantity in cart reduced")
+        cart.emptyCart()
+
+    })
+
+    it("should calculate the total for items in cart", () => {
+        const cart = new Cart();
+        const item1 = new Item({id:1, name:"airpods", 
+        price:5000, quantity:10});
+        const item2 = new Item({id:2, name: "charger",
+        price: 1500, quantity: 5})
+
+        cart.addItem(item1, 2)
+        cart.addItem(item2, 1)
+        const response = cart.calculateTotal()
+
+        expect(response).toEqual(11500)
+        cart.emptyCart()
 
     })
 })
